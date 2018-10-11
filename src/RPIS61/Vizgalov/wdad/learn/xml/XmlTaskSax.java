213dd371fbf003447e7240eeec5e7e487b7714ce
Mx_dev <none@none.none>
2018-10-11 23:02:51 +0300

    XmlTaskSax: 1 method don't work

--- src/RPIS61/Vizgalov/wdad/learn/xml/XmlTaskSax.java
+++ src/RPIS61/Vizgalov/wdad/learn/xml/XmlTaskSax.java
@@ -1,72 +1,125 @@
-package wdad.learn.xml;
+package RPIS61.Vizgalov.wdad.learn.xml;
+
+import java.util.ArrayList;
+
+import java.lang.UnsupportedOperationException;
 
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
-import org.xml.sax.helpers.DefaultHendler;
+import org.xml.sax.helpers.DefaultHandler;
 
 public class XmlTaskSax {
 	final String fileName = "organization.xml";
 	SAXParserFactory factory;
 	SAXParser saxParser;
-	DefaultHendler testHandler;
-	
+
 	public XmlTaskSax() {
 		try {
 			factory = SAXParserFactory.newInstance();
 			saxParser = factory.newSAXParser();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-			testHandler = new DefaultHendler() {
-				boolean organization = false;
+	public int salaryAverage() {	
+		DefaultHandler salaryAverageHandler;
+		ArrayList<Integer> salaries = new ArrayList<Integer>();
+
+		try {
+			salaryAverageHandler = new DefaultHandler() {
+				boolean salary = false;
 
 				@Override
 				public void startElement(String uri, String localName, String qName,
 						Attributes attributes) throws SAXException {
-					if (qName.equalsIgnoreCase("organization")) {
-						organization = true;
+					if (qName.equalsIgnoreCase("salary")) {
+						salary = true;
 					}
 				}
 
 				@Override
 				public void characters(char ch[], int start, int length)
 						throws SAXException {
-					if (organization) {
-						System.out.println("organization: " + new String(ch, start, length));
-						organization = false;
+					if (salary) {
+						salaries.add(new Integer(new String(ch, start, length)));
+						salary = false;
 					} 
 				}
 			};
+			saxParser.parse(fileName, salaryAverageHandler);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	}
-
-	public void test() {
-		saxParser.parse(fileName, testHandler);
-	}
-
-	public int salaryAverage() {
 		
-		return -1;
+		int average = 0;
+		for(int i : salaries)
+			average += i;
+		return average / salaries.size();
 	}
 	
 	public int salaryAverage(String departmentName) {
+		DefaultHandler salaryAverageHandler;
+		ArrayList<Integer> salaries = new ArrayList<Integer>();
+
+		try {
+			salaryAverageHandler = new DefaultHandler() {
+				boolean salary = false;
+				boolean necessaryDepartment = false;
+
+				@Override
+				public void startElement(String uri, String localName, String qName,
+						Attributes attributes) throws SAXException {
+					if (qName.equalsIgnoreCase("salary")) {
+						salary = true;
+					}
+					if (qName.equalsIgnoreCase("department")
+							&& attributes.getValue("name").equals(departmentName)) {
+						necessaryDepartment = true;
+					}
+				}
+
+				@Override
+				public void characters(char ch[], int start, int length)
+						throws SAXException {
+					if (necessaryDepartment && salary) {
+						salaries.add(new Integer(new String(ch, start, length)));
+						salary = false;
+					} 
+				}
+
+				@Override
+				public void endElement(String uri, String localName, String qName)
+						throws SAXException {
+					if (qName.equalsIgnoreCase("department")) {
+						necessaryDepartment = false;
+					}
+				}
+			};
+			saxParser.parse(fileName, salaryAverageHandler);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
-		return -1;
+		int average = 0;
+		for(int i : salaries)
+			average += i;
+		return average / salaries.size();
 	}
 	
 	public void setJobTitle(String firstName, String secondName,
 			String newJobTitle) {
-		System.out.println("Not supported!");
+		throw new UnsupportedOperationException("SAX can't write.");
 	}
 	
 	public void setSalary(String firstName, String secondName, int newSalary) {
-		System.out.println("Not supported!");
+		throw new UnsupportedOperationException("SAX can't write.");
 	}
 	
 	public void fireEmployee(String firstName, String secondName) {
-		;
+		throw new UnsupportedOperationException("SAX can't write.");
 	}
 }
--- src/RPIS61/Vizgalov/wdad/learn/xml/XmlTest.java
+++ src/RPIS61/Vizgalov/wdad/learn/xml/XmlTest.java
@@ -1,7 +1,14 @@
-package wdad.learn.xml;
+package RPIS61.Vizgalov.wdad.learn.xml;
+
+import RPIS61.Vizgalov.wdad.learn.xml.XmlTaskSax;
 
 public class XmlTest {
 	public static void main(String[] args) {
 		System.out.println("XmlTest is run.");
+
+		XmlTaskSax xmlTaskSax = new XmlTaskSax();
+		System.out.println("Salary average: " + xmlTaskSax.salaryAverage());
+		System.out.println("Salary average in \"Department 1.2\": "
+				+ xmlTaskSax.salaryAverage("Department 1.2"));
 	}
 }
