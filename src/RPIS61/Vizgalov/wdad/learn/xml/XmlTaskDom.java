499f4173f66f065dc5b7a2ac166be37be99d5873
Mx_dev <none@none.none>
2018-10-12 15:23:44 +0300

    XmlTaskDom complete

--- src/RPIS61/Vizgalov/wdad/learn/xml/XmlTaskDom.java
+++ src/RPIS61/Vizgalov/wdad/learn/xml/XmlTaskDom.java
@@ -1,26 +1,223 @@
 package RPIS61.Vizgalov.wdad.learn.xml;
 
+import java.io.FileOutputStream;
+import java.io.File;
+import java.io.IOException;
+
+import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.DOMException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.NamedNodeMap;
+import org.xml.sax.SAXException;
+
 public class XmlTaskDom {
+	DocumentBuilder documentBuilder;
+	Document document;
+	final String fileName = "organization.xml";
+
+	public XmlTaskDom() {
+		try {
+			documentBuilder = DocumentBuilderFactory.newInstance()
+					.newDocumentBuilder();
+			document = documentBuilder.parse(fileName);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 	public int salaryAverage() {
+		ArrayList<Integer> salaries = new ArrayList<Integer>();
 		
-		return -1;
+		try {
+			Node root = document.getDocumentElement(); // <organization>
+			NodeList departments = root.getChildNodes();
+			for(int i = 0; i < departments.getLength(); i++) {
+				Node department = departments.item(i);
+				NodeList employees = department.getChildNodes();
+				for(int j = 0; j < employees.getLength(); j++) {
+					Node employee = employees.item(j);
+					NodeList employeeProperties = employee.getChildNodes();
+					for(int k = 0; k < employeeProperties.getLength(); k++) {
+						Node employeeProperty = employeeProperties.item(k);
+						if (employeeProperty.getNodeName().equals("salary"))
+							salaries.add(new Integer(employeeProperty.getTextContent()));
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		int average = 0;
+		for(int i : salaries)
+			average += i;
+		return average / salaries.size();
 	}
 	
 	public int salaryAverage(String departmentName) {
+		ArrayList<Integer> salaries = new ArrayList<Integer>();
 		
-		return -1;
+		try {
+			Node root = document.getDocumentElement(); // <organization>
+			NodeList departments = root.getChildNodes();
+			for(int i = 0; i < departments.getLength(); i++) {
+				Node department = departments.item(i);
+				NamedNodeMap attributes = department.getAttributes();
+				if (attributes != null) {
+					Node attribute = attributes.getNamedItem("name");
+					if (attribute != null
+							&& attribute.getNodeValue().equals(departmentName)) {
+						NodeList employees = department.getChildNodes();
+						for(int j = 0; j < employees.getLength(); j++) {
+							Node employee = employees.item(j);
+							NodeList employeeProperties = employee.getChildNodes();
+							for(int k = 0; k < employeeProperties.getLength(); k++) {
+								Node employeeProperty = employeeProperties.item(k);
+								if (employeeProperty.getNodeName().equals("salary")) {
+									salaries.add(new Integer(employeeProperty
+											.getTextContent()));
+								}
+							}
+						}
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		int average = 0;
+		for(int i : salaries)
+			average += i;
+		return average / salaries.size();
 	}
 	
 	public void setJobTitle(String firstName, String secondName,
 			String newJobTitle) {
-		;
+		try {
+			Node root = document.getDocumentElement(); // <organization>
+			NodeList departments = root.getChildNodes();
+			for(int i = 0; i < departments.getLength(); i++) {
+				Node department = departments.item(i);
+				NodeList employees = department.getChildNodes();
+				for(int j = 0; j < employees.getLength(); j++) {
+					Node employee = employees.item(j);
+					NamedNodeMap attributes = employee.getAttributes();
+					if (attributes != null) {
+						Node attrFirstname = attributes.getNamedItem("firstname");
+						Node attrSecondname = attributes.getNamedItem("secondname");
+						if (attrFirstname != null && attrSecondname != null
+								&& attrFirstname.getNodeValue().equals(firstName)
+								&& attrSecondname.getNodeValue().equals(secondName)) {
+							NodeList employeeProperties = employee.getChildNodes();
+							for(int k = 0; k < employeeProperties.getLength(); k++) {
+								Node employeeProperty = employeeProperties.item(k);
+								if (employeeProperty.getNodeName().equals("jobtitle")) {
+									NamedNodeMap attrsJobTitle = employeeProperty.getAttributes();
+									if (attrsJobTitle != null) {
+										Node attrJobTitle = attrsJobTitle.getNamedItem("value");
+										if (attrJobTitle != null) {
+											attrJobTitle.setNodeValue(newJobTitle);
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+			writeXml();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void setSalary(String firstName, String secondName, int newSalary) {
-		;
+		try {
+			Node root = document.getDocumentElement(); // <organization>
+			NodeList departments = root.getChildNodes();
+			for(int i = 0; i < departments.getLength(); i++) {
+				Node department = departments.item(i);
+				NodeList employees = department.getChildNodes();
+				for(int j = 0; j < employees.getLength(); j++) {
+					Node employee = employees.item(j);
+					NamedNodeMap attributes = employee.getAttributes();
+					if (attributes != null) {
+						Node attrFirstname = attributes.getNamedItem("firstname");
+						Node attrSecondname = attributes.getNamedItem("secondname");
+						if (attrFirstname != null && attrSecondname != null
+								&& attrFirstname.getNodeValue().equals(firstName)
+								&& attrSecondname.getNodeValue().equals(secondName)) {
+							NodeList employeeProperties = employee.getChildNodes();
+							for(int k = 0; k < employeeProperties.getLength(); k++) {
+								Node employeeProperty = employeeProperties.item(k);
+								if (employeeProperty.getNodeName().equals("salary")) {
+									employeeProperty.getFirstChild().setNodeValue(String.valueOf(newSalary));
+								}
+							}
+						}
+					}
+				}
+			}
+			writeXml();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void fireEmployee(String firstName, String secondName) {
-		;
+		try {
+			Node root = document.getDocumentElement(); // <organization>
+			NodeList departments = root.getChildNodes();
+			for(int i = 0; i < departments.getLength(); i++) {
+				Node department = departments.item(i);
+				NodeList employees = department.getChildNodes();
+				for(int j = 0; j < employees.getLength(); j++) {
+					Node employee = employees.item(j);
+					NamedNodeMap attributes = employee.getAttributes();
+					if (attributes != null) {
+						Node attrFirstname = attributes.getNamedItem("firstname");
+						Node attrSecondname = attributes.getNamedItem("secondname");
+						if (attrFirstname != null && attrSecondname != null
+								&& attrFirstname.getNodeValue().equals(firstName)
+								&& attrSecondname.getNodeValue().equals(secondName)) {
+							department.removeChild(employee);
+						}
+					}
+				}
+			}
+			writeXml();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void writeXml() {
+		try {
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			DOMSource source = new DOMSource(document);
+			StreamResult result = new StreamResult(new File(fileName));
+			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+			transformer.transform(source, result);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
--- src/RPIS61/Vizgalov/wdad/learn/xml/XmlTest.java
+++ src/RPIS61/Vizgalov/wdad/learn/xml/XmlTest.java
@@ -1,14 +1,41 @@
 package RPIS61.Vizgalov.wdad.learn.xml;
 
 import RPIS61.Vizgalov.wdad.learn.xml.XmlTaskSax;
+import RPIS61.Vizgalov.wdad.learn.xml.XmlTaskDom;
 
 public class XmlTest {
 	public static void main(String[] args) {
 		System.out.println("XmlTest is run.");
 
+		System.out.println("   SAX:");
 		XmlTaskSax xmlTaskSax = new XmlTaskSax();
 		System.out.println("Salary average: " + xmlTaskSax.salaryAverage());
+		
+		// Throw exception: java.lang.NumberFormatException: For input string: "
+		//System.out.println("Salary average in \"Department 1.2\": "
+		//		+ xmlTaskSax.salaryAverage("Department 1.2"));
+		
+		// Throw exception: java.lang.UsupportedOperationException: SAX can't write.
+		//xmlTaskSax.setJobTitle("firstname 1.1.4", "secondname 1.1.4", "engineer");
+		
+		// Throw exception: java.lang.UsupportedOperationException: SAX can't write.
+		//xmlTaskSax.setSalary("firstname 1.1.4", "secondname 1.1.4", 85034);
+	
+		// Throw exception: java.lang.UsupportedOperationException: SAX can't write.
+		//xmlTaskSax.fireEmployee("firstname 1.1.4", "secondname 1.1.4");
+		
+		
+		System.out.println("   DOM:");
+		XmlTaskDom xmlTaskDom = new XmlTaskDom();
+		System.out.println("Salary average: " + xmlTaskDom.salaryAverage());
+		
 		System.out.println("Salary average in \"Department 1.2\": "
-				+ xmlTaskSax.salaryAverage("Department 1.2"));
+				+ xmlTaskDom.salaryAverage("Department 1.2"));
+
+		xmlTaskDom.setJobTitle("firstname 1.3.2", "secondname 1.3.2", "assistant");
+		
+		xmlTaskDom.setSalary("firstname 1.3.2", "secondname 1.3.2", 333333);
+	
+		xmlTaskDom.fireEmployee("firstname 1.1.2", "secondname 1.1.2");
 	}
 }
