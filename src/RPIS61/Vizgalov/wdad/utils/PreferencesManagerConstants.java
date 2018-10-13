19e6c623860e3bb4637dc00b9c07e67ffa2c5516
Mx_dev <none@none.none>
2018-10-13 21:54:31 +0300

    task 1 complete, but not merged becouse previous commits don't merged

---
+++ src/RPIS61/Vizgalov/wdad/utils/PreferencesManagerConstants.java
@@ -0,0 +1,11 @@
+package RPIS61.Vizgalov.wdad.utils;
+
+public interface PreferencesManagerConstants {
+	final String CREATE_REGISTRY = "appconfig.rmi.server.registry.createregistry";
+	final static String REGISTRY_ADDRESS
+			= "appconfig.rmi.server.registry.registryaddress";
+	final String REGISTRY_PORT = "appconfig.rmi.server.registry.registryport";
+	final String POLICY_PATH = "appconfig.rmi.client.policypath";
+	final String USE_CODE_BASE_ONLY = "appconfig.rmi.client.usecodebaseonly";
+	final String CLASS_PROVIDER = "appconfig.rmi.classprovider";
+}
