2c280589c03700da1f15e1a870e8637d344c367e
Foo Bar <foo@foo.foo>
2018-12-11 22:42:11 +0300

    DB created

---
+++ db/createdb.sh
@@ -0,0 +1,2 @@
+mysql -u root -h localhost -p -e "CREATE DATABASE organization;"
+mysql -u root -h localhost -p organization < organization.sql 
---
+++ db/organization.sql
@@ -0,0 +1,28 @@
+CREATE TABLE employees (
+	id INT,
+	first_name VARCHAR(50),
+	second_name VARCHAR(50),
+	birth_date DATE,
+	hire_date DATE,
+	salary NUMERIC(15,5),
+	jobtitles_id INT,
+	departments_id INT,
+	PRIMARY KEY(id)
+);
+CREATE TABLE departments (
+	id INT,
+	name VARCHAR(50),
+	Description VARCHAR(1),
+	PRIMARY KEY(id)
+);
+CREATE TABLE jobtitles (
+	id INT,
+	name VARCHAR(100),
+	PRIMARY KEY(id)
+);
+ALTER TABLE employees
+	ADD FOREIGN KEY (jobtitles_id)
+	REFERENCES jobtitles (id);
+ALTER TABLE employees
+	ADD FOREIGN KEY (departments_id)
+	REFERENCES departments (id);
