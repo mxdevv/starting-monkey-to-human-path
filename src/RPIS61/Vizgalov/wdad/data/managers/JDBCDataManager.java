2a5ece5c2c0797986ae77ef388ac630d06539db9
WildChildCode <stefanov-m@yandex.ru>
2018-12-20 14:54:13 +0400

    Employee.birthDate

--- src/RPIS61/Vizgalov/wdad/data/managers/JDBCDataManager.java
+++ src/RPIS61/Vizgalov/wdad/data/managers/JDBCDataManager.java
@@ -3,14 +3,13 @@ package RPIS61.Vizgalov.wdad.data.managers;
 import java.lang.StringBuilder;
 
 import java.rmi.RemoteException;
+import java.sql.*;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 import javax.sql.DataSource;
-import java.sql.Statement;
-import java.sql.ResultSet;
-import java.sql.PreparedStatement;
-import java.sql.Connection;
 
 import RPIS61.Vizgalov.wdad.learn.rmi.JobTitle;
 import RPIS61.Vizgalov.wdad.learn.rmi.Employee;
@@ -25,7 +24,6 @@ public class JDBCDataManager implements DataManager {
 	public JDBCDataManager() {
 		try {
 			dataSource = DataSourceFactory.createDataSource();
-			//todo new connection per method
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -33,7 +31,6 @@ public class JDBCDataManager implements DataManager {
 	
 	/* non-interface methods */
 	private String sql_WHERE(Employee employee) {
-		//todo юзай StringBuilder
     StringBuilder strBuild = new StringBuilder(150);
 		strBuild.append("WHERE ( ")
 		        .append("  first_name = \'")
@@ -55,7 +52,6 @@ public class JDBCDataManager implements DataManager {
 		return strBuild.toString();
 	}
 	public int getJobTitleId(JobTitle jobTitle) {
-		//todo select to Jobtitles
 		Connection connection = null;
 		PreparedStatement statement = null;
 		ResultSet resultSet = null;
@@ -100,22 +96,20 @@ public class JDBCDataManager implements DataManager {
 		PreparedStatement statement = null;
 		ResultSet resultSet = null;
 		try {
-			//todo используй preparedStatement как PreparedStatement, а не как Statement
 			connection = dataSource.getConnection();
 			statement = connection.prepareStatement(
 				"INSERT INTO employees ( " +
-					"id, first_name, second_name, birth_date, hire_date, salary, " +
+					"first_name, second_name, hire_date, salary, " +
 					"jobtitles_id, departments_id ) VALUES ( " +
-					"NULL, ?, ?, NULL, ?, ?, ?, ? );"
+					"?, ?, ?, ?, ?, ?, ? );"
 			);
 			statement.setString(1, employee.firstName);
 			statement.setString(2, employee.secondName);
-			statement.setString(3,
-				new SimpleDateFormat("yyyy-MM-dd").format(
-					employee.hiredate));
-			statement.setInt(4, employee.salary);
-			statement.setInt(5, getJobTitleId(employee.jobTitle));
-			statement.setInt(6, departmentsId);
+			statement.setDate(3, new java.sql.Date(employee.birthdate.getTime()));
+			statement.setDate(4, new java.sql.Date(employee.hiredate.getTime()));
+			statement.setInt(5, employee.salary);
+			statement.setInt(6, getJobTitleId(employee.jobTitle));
+			statement.setInt(7, departmentsId);
 			
 			statement.executeUpdate();
 		} catch (Exception e) {
@@ -134,7 +128,7 @@ public class JDBCDataManager implements DataManager {
 		try {
 			connection = dataSource.getConnection();
 			statement = connection.prepareStatement(
-				"SELECT AVG(salary) FROM employees"); //todo используй агрегатные функции
+				"SELECT AVG(salary) FROM employees");
 			resultSet = statement.executeQuery();
 			resultSet.next();
 			average = resultSet.getInt(1);
@@ -151,16 +145,13 @@ public class JDBCDataManager implements DataManager {
 		ResultSet resultSet = null;
 		int average = -1;
 		try {
-			//todo используй агрегатные функции
-			//todo используй preparedStatement как PreparedStatement, а не как Statement
 			connection = dataSource.getConnection();
 			statement = connection.prepareStatement(
  				"SELECT AVG(salary) FROM employees " +
-				"WHERE departments_id = ANY( "       +
+				"WHERE departments_id IN ( "       +
 				"  SELECT id FROM departments "      +
-				"  WHERE name = ? " +
-        ")"
-      );
+				"  WHERE name = ?)"
+			);
 			statement.setString(1, departmentName);
 			resultSet = statement.executeQuery();
 		  resultSet.next();
@@ -178,7 +169,6 @@ public class JDBCDataManager implements DataManager {
 		PreparedStatement statement = null;
 		ResultSet resultSet = null;
 		try {
-			//todo используй preparedStatement как PreparedStatement, а не как Statement
       int id = getJobTitleId(newJobTitle);
 			connection = dataSource.getConnection();
 			statement = connection.prepareStatement(
@@ -199,7 +189,6 @@ public class JDBCDataManager implements DataManager {
 		PreparedStatement statement = null;
 		ResultSet resultSet = null;
 		try {
-			//todo используй preparedStatement как PreparedStatement, а не как Statement
 			connection = dataSource.getConnection();
 			statement = connection.prepareStatement(
 				"UPDATE employees SET salary = ? " +
@@ -218,7 +207,6 @@ public class JDBCDataManager implements DataManager {
 		PreparedStatement statement = null;
 		ResultSet resultSet = null;
 		try {
-			//todo используй preparedStatement как PreparedStatement, а не как Statement
 			connection = dataSource.getConnection();
 			statement = connection.prepareStatement(
 				"DELETE FROM employees " +
@@ -237,11 +225,9 @@ public class JDBCDataManager implements DataManager {
 		PreparedStatement statement = null;
 		ResultSet resultSet = null;
 		try {
-			//todo автоинкремент для первичных ключей
 			connection = dataSource.getConnection();
-			//todo используй preparedStatement как PreparedStatement, а не как Statement
 			statement = connection.prepareStatement(
-				"INSERT INTO departments (id, name, Description) VALUES ( NULL, ?, 'x' );"
+				"INSERT INTO departments (name, Description) VALUES ( ?, 'x' );"
 			);
 			statement.setString(1, department.name);
 			statement.executeUpdate();
